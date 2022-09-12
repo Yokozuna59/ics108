089b090e693be39ec2b7b633006d6577f330d8ed
Yokozuna59 <u.yokozuna@gmail.com>
2022-09-12 12:34:03 +0300

    push lab04
    Signed-off-by: Yokozuna59 <u.yokozuna@gmail.com>

---
+++ labs/lab04/README.md
@@ -0,0 +1,28 @@
+# Lab 04
+
+## Task 1
+
+1. **Question Discription:**
+
+    Write the following method that returns true if the array is already sorted in nondecreasing order:
+
+    ```java
+    public static boolean isSorted(int[] array)
+    ```
+
+    Write a test program that prompts the user to enter an array and displays whether the array is sorted or not **without sorting it**.
+
+2. **Sample Runs** (user input is in bold):
+
+    Enter the size of the array: **8**<br>
+    Enter the contents of the array: **10 1 5 16 61 9 11 1**<br>
+    The array has 8 integers 10 1 5 16 61 9 11 1<br>
+    The array is not sorted
+
+    ---
+    Enter the size of the array: **10**<br>
+    Enter the contents of the array: **1 1 3 4 4 5 7 9 11 21**<br>
+    The array has 10 integers 1 1 3 4 4 5 7 9 11 21<br>
+    The array is sorted
+
+**My Solution**: [TaskOne.java](TaskOne.java)
---
+++ labs/lab04/TaskOne.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class TaskOne {
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length - 1; i++) {
+            if (!(array[i - 1] <= array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int arraySize = input.nextInt();
+
+        int[] array = new int[arraySize];
+        System.out.print("Enter the contents of the array: ");
+
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = input.nextInt();
+        }
+
+        System.out.printf("The array has %d integers:", arraySize);
+        for (int i = 0; i < arraySize; i++) {
+            System.out.print(" " + array[i]);
+        }
+
+        boolean sorted = TaskOne.isSorted(array);
+
+        if (sorted)
+            System.out.println("\nThe array is sorted");
+        else
+            System.out.println("\nThe array is not sorted");
+
+        input.close();
+    }
+}
